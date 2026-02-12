664db10b69ff6e7ec22f839f17d0bea5cf9b6998
Shiv <shivbhagat@icloud.com>
2026-02-12 12:02:52 -0600

    Fix Bounded component to support refs for Hero animations

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -8,21 +8,21 @@ type BoundedProps = {
 	[key: string]: any;
 };
 
-const Bounded: React.FC<BoundedProps> = ({
-	as,
-	className,
-	children,
-	...restProps
-}) => {
-	const Comp = as || "section";
-	return (
-		<Comp
-			className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-			{...restProps}
-		>
-			<div className="mx-auto w-full max-w-7xl">{children}</div>
-		</Comp>
-	);
-};
+const Bounded = React.forwardRef<HTMLElement, BoundedProps>(
+	({ as, className, children, ...restProps }, ref) => {
+		const Comp: any = as || "section";
+		return (
+			<Comp
+				ref={ref}
+				className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+				{...restProps}
+			>
+				<div className="mx-auto w-full max-w-7xl">{children}</div>
+			</Comp>
+		);
+	},
+);
+
+Bounded.displayName = "Bounded";
 
 export default Bounded;
