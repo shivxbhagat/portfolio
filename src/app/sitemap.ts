75a92d649b2e5300f30bf0e2a9902be8c6010320
Shiv <shivbhagat@icloud.com>
2026-02-12 11:51:51 -0600

    Remove Prismic dependencies and API routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,16 @@
 import { MetadataRoute } from "next";
-import { createClient } from "@/data/client";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const client = createClient();
-	const pages = client.getAllByType("page");
-	const projects = client.getAllByType("project");
-
+export default function sitemap(): MetadataRoute.Sitemap {
 	const siteRoot = "https://demo.com";
 
-	const homepageRoute = {
-		url: siteRoot,
-		lastModified: new Date().toISOString(),
-	};
-
-	const pagesRoutes = pages.map((page) => ({
-		url: siteRoot + "/" + page.uid,
-		lastModified: new Date().toISOString(),
-	}));
-
-	const projectsRoutes = projects.map((project) => ({
-		url: siteRoot + "/project/" + project.uid,
-		lastModified: project.last_publication_date || new Date().toISOString(),
-	}));
-
-	return [homepageRoute, ...pagesRoutes, ...projectsRoutes];
+	return [
+		{
+			url: siteRoot,
+			lastModified: new Date().toISOString(),
+		},
+		{
+			url: `${siteRoot}/about`,
+			lastModified: new Date().toISOString(),
+		},
+	];
 }
