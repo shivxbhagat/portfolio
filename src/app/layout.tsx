59cfb03d7001593b790f541918e50bfa838fee56
Shiv <shivbhagat@icloud.com>
2026-02-12 14:05:31 -0600

    Add Vercel Speed Insights and Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import clsx from "clsx";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import { createClient } from "@/data/client";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -10,29 +12,31 @@ import Header from "@/components/Header";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export async function generateMetadata(): Promise<Metadata> {
-  const client = createClient();
-  const settings = client.getSingle("settings");
+	const client = createClient();
+	const settings = client.getSingle("settings");
 
-  return {
-    title: settings.data.meta_title,
-    description: settings.data.meta_description,
-  };
+	return {
+		title: settings.data.meta_title,
+		description: settings.data.meta_description,
+	};
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="bg-slate-900">
-      <body className={clsx(urbanist.className, "relative min-h-screen")}>
-        <Header />
-        {children}
-        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
-        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="bg-slate-900">
+			<body className={clsx(urbanist.className, "relative min-h-screen")}>
+				<Header />
+				{children}
+				<div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+				<div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
+				<Footer />
+				<SpeedInsights />
+				<Analytics />
+			</body>
+		</html>
+	);
 }
