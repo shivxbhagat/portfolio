c3bcdf782ad0b9d27cd81877200635d6400a96f2
Shiv <shivbhagat@icloud.com>
2026-02-12 12:10:06 -0600

    Clean deployment - remove all Prismic remnants

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.gov.mb.ca",
-      },
-      {
-        protocol: "https",
-        hostname: "news.umanitoba.ca",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+			},
+			{
+				protocol: "https",
+				hostname: "www.gov.mb.ca",
+			},
+			{
+				protocol: "https",
+				hostname: "news.umanitoba.ca",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -14,7 +14,10 @@ const Bounded = React.forwardRef<HTMLElement, BoundedProps>(
 		return (
 			<Comp
 				ref={ref}
-				className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+				className={clsx(
+					"px-4 py-10 md:px-6 md:py-14 lg:py-16",
+					className,
+				)}
 				{...restProps}
 			>
 				<div className="mx-auto w-full max-w-7xl">{children}</div>
