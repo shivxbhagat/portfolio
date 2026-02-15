e6dbc8b6ba3e18106adbe4d73b421dfa272775a0
Shiv <shivbhagat@icloud.com>
2026-02-15 11:51:01 -0600

    Fix Biography grid layout for desktop view

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -24,16 +24,16 @@ const Biography = ({ slice }: { slice: BiographySlice }): JSX.Element => {
 			data-slice-variation="default"
 		>
 			<div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
-				<Heading size="xl" className="col-start-1 order-1">
+				<Heading size="xl" className="col-start-1 md:row-start-1">
 					{slice.primary.heading}
 				</Heading>
 
 				<Avatar
 					image={slice.primary.avatar}
-					className="max-w-xs order-2 md:order-none md:col-start-2 md:row-start-1 md:row-end-3"
+					className="max-w-xs md:col-start-2 md:row-start-1 md:row-end-4"
 				/>
 
-				<div className="prose prose-xl prose-slate prose-invert col-start-1 order-3 md:order-none">
+				<div className="prose prose-xl prose-slate prose-invert col-start-1 md:row-start-2">
 					{slice.primary.body.map((para, index) => (
 						<p key={index}>{para.text}</p>
 					))}
@@ -42,7 +42,7 @@ const Biography = ({ slice }: { slice: BiographySlice }): JSX.Element => {
 					linkField={slice.primary.button_link}
 					label={slice.primary.button_text}
 					target="_blank"
-					className="order-4 md:order-none"
+					className="col-start-1 md:row-start-3"
 				/>
 			</div>
 		</Bounded>
